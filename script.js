9dcb5c2111276ce286cc09abba985cea1c704c3f
Ayushi Sahoo <119499621+ayushisahoo@users.noreply.github.com>
2023-09-02 23:01:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var tl = gsap.timeline()
+
+tl.from("#nav img, #nav h3, #nav h4, #nav button",{
+    y:-100,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger: 0.2
+})
+
+tl.from("#main h1",{
+    opacity:0,
+    x:-200,
+    stagger:0.3
+})
+
+tl.from("#main>img",{
+    scale:0,
+    opacity:0,
+    stagger: 0.2
+})
+
+tl.from("h5",{
+    scale:0,
+    opacity:0,
+
+})
+
+tl.to("h5",{
+     y:40,
+     repeat:-1,
+     duration:1,
+     yoyo: true
+})
